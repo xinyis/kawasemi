487a908eec20600fc5d6702f00e0cc8ce1c96757
Ojima Hikaru <ojiam.h@gmail.com>
2013-05-21 15:54:12 +0900

    Fix memo saving flow.

--- stock.js
+++ stock.js
@@ -1,5 +1,22 @@
+/*jshint strict:true */
+/*jshint browser:true, jquery:true, devel:true */
+/*global google:false, _:false */
+
 
 // ロード時に自動でカーソルを合わせる
-$(document).ready( function() {
-  $("#comment_textarea").focus()
-});
+(function() {
+    "use strict";
+    
+    $(document).ready( function() {
+        $("#comment_textarea").focus();
+
+        $("#picture-button").on('click', function () {
+            alert('写真をとりました');
+            $(".memo-stock").append('<li><img src="img/daruma.jpg" /></li>');
+        });
+
+        $("#stock_form").submit( function () {
+            alert('メモを保存しました');
+        });
+    });
+})();
