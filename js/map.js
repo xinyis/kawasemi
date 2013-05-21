39d8ffbeae5ac825875a983dc0920bc9308a24aa
孫 辛夷 <xinyi.sun@90201714n.intra.emercury.co.jp>
2013-05-21 19:27:59 +0900

    現在地icon

--- js/map.js
+++ js/map.js
@@ -13,13 +13,15 @@
             center: myLatlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
+    
+        var image = 'img/gps.png';
         document.body.onload = function () {
             map.app = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
             var marker = new google.maps.Marker({
                 position: myLatlng,
                 map: map.app,
+                icon: image,
                 title:"Hello World!"
             });
             $(window).trigger('mapinitialized', this);
